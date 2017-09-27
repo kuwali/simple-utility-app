f5a84df615eae47cc27b46d6154e73c6861759ec
kuwali <kustiawanto.halim@gmail.com>
2017-09-27 18:25:20 +0700

    Add test

---
+++ test/prettydate-test.js
@@ -0,0 +1,8 @@
+const prettyDate = require('../src/prettyDate');
+const QUnit = require('qunitjs');
+
+QUnit.test("prettydate basics", function( assert ) {
+  assert.equal(prettyDate(20), "just now");
+  assert.equal(prettyDate(100), "1 minute ago");
+  assert.equal(prettyDate(7000), "1 hour ago");
+});
