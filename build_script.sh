dd3e82b59b1c31a26541868ce6925f11043ac0f3
kuwali <kustiawanto.halim@gmail.com>
2017-09-27 22:14:46 +0700

    Add build script

---
+++ build_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+zip -r ./build/app.nw * -x /build/\*
+wget -O ./build/nw.tar.gz https://dl.nwjs.io/v0.25.3/nwjs-v0.25.3-linux-x64.tar.gz
+mkdir ./build/nw
+tar -xvzf ./build/nw.tar.gz --strip 1 -C nw
+rm ./build/nw.tar.gz
+cat ./build/nw/nw ./build/app.nw > ./build/app && chmod +x ./build/app 
