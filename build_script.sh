0f0aef2d8fe7a93cf43be684c159a659a3d8fb90
kuwali <kustiawanto.halim@gmail.com>
2017-09-27 22:32:45 +0700

    Update typo

--- build_script.sh
+++ build_script.sh
@@ -4,6 +4,6 @@ mkdir build
 zip -r ./build/app.nw * -x /build/\*
 wget -O ./build/nw.tar.gz https://dl.nwjs.io/v0.25.3/nwjs-v0.25.3-linux-x64.tar.gz
 mkdir ./build/nw
-tar -xvzf ./build/nw.tar.gz --strip 1 -C nw
+tar -xvzf ./build/nw.tar.gz --strip 1 -C ./build/nw
 rm ./build/nw.tar.gz
 cat ./build/nw/nw ./build/app.nw > ./build/app && chmod +x ./build/app 
